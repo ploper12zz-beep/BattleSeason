bc92af794d79f201d4ea8fb61d484ac6c6970a16
ploper12zz-beep <ploper12zz@gmail.com>
2026-09-23 14:54:00 +0700

    declaring variable and displaying input data

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int totalexp = 0;
+
+Console.Write("input your name: ");
+string name = Console.ReadLine();
+Console.Write("input your battle number: ");
+int battlenumber = int.Parse(Console.ReadLine());
+Console.Write("input the EXP gained per battle: ");
+int expPerBattle = int.Parse(Console.ReadLine());
+
